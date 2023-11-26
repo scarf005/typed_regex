ae08a179bcced1b1c097162d380c6a8484225298
scarf <greenscarf005@gmail.com>
2023-11-26 22:40:19 +0900

    feat!: migrate to deno

--- README.md
+++ README.md
@@ -1,56 +1,53 @@
 # typed-regex
+
 A typescript library for writing type-safe regular expressions using [named capture groups](https://github.com/tc39/proposal-regexp-named-groups).
 
-![GitHub Workflow Status (main)](https://img.shields.io/github/workflow/status/phenax/typed-regex/Node.js%20CI/main)
-[![npm](https://img.shields.io/npm/v/typed-regex)](https://www.npmjs.com/package/typed-regex)
-[![npm bundle size](https://img.shields.io/bundlephobia/minzip/typed-regex)](https://www.npmjs.com/package/typed-regex)
-
-
+![GitHub Workflow Status (main)](https://img.shields.io/github/workflow/status/phenax/typed-regex/Node.js%20CI/main) [![npm](https://img.shields.io/npm/v/typed-regex)](https://www.npmjs.com/package/typed-regex) [![npm bundle size](https://img.shields.io/bundlephobia/minzip/typed-regex)](https://www.npmjs.com/package/typed-regex)
 
 ## Install
+
 To install the latest stable version of typed-regex -
+
 ```
 yarn add typed-regex
 // OR
 npm install --save typed-regex
 ```
 
-
 ## Usage
+
 The type of the result object is infered from the regular expression.
 
 ```ts
-import { TypedRegEx } from 'typed-regex';
+import { TypedRegEx } from "typed-regex"
 
-const regex = TypedRegEx('^(?<year>\\d{4})-(?<month>\\d{2})-(?<day>\\d{2})$', 'g');
-const result = regex.captures('2020-12-02');
+const regex = TypedRegEx("^(?<year>\\d{4})-(?<month>\\d{2})-(?<day>\\d{2})$", "g")
+const result = regex.captures("2020-12-02")
 
 result // : undefined | { year: string, month: string, day: string }
 ```
 
 > NOTE: The regular expression has to be a string literal for the types to be valid
 
-
 #### Optional properties
+
 If the capture group is marked as optional in the regular expression, the generated type will reflect that
 
 ```ts
-const regex = TypedRegEx('(?<first>\\d+)/(?<second>\\w+)?', 'g');
-const result = regex.captures('1234/foobar');
+const regex = TypedRegEx("(?<first>\\d+)/(?<second>\\w+)?", "g")
+const result = regex.captures("1234/foobar")
 
 result // : undefined | { first: string, second?: string }
 ```
 
-
 #### API Docs
+
 [You can find more information about the library in the API documentation](https://github.com/phenax/typed-regex/blob/main/docs/API.md)
 
-
 ## Browser support
+
 Named capture groups are supported in [these browsers](https://caniuse.com/mdn-javascript_builtins_regexp_named_capture_groups)
 
-
-
 ## License
-Typed-Regex is licensed under [MIT](./LICENSE)
 
+Typed-Regex is licensed under [MIT](./LICENSE)
---
+++ main.ts
@@ -0,0 +1,85 @@
+// deno-lint-ignore-file no-explicit-any ban-types
+// TODO: (bug) nested * is not set as optional
+// TODO: Create some parse errors in invalid cases
+// TODO: Parse normal captures in a typed tuple?
+// TODO: Ignore non-capturing groups
+
+type ReError<T extends string> = { type: T }
+
+// Ref: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/RegExp/RegExp
+type Flag = "d" | "g" | "i" | "m" | "s" | "u" | "y"
+
+// deno-fmt-ignore
+type FlagChecker<Fl extends string> =
+    Fl extends ""
+        ? string
+	    : Fl extends `${Flag}${infer rest}`
+            ? FlagChecker<rest>
+	        : ReError<`Invalid flag used: ${Fl}`>
+
+// deno-fmt-ignore
+export type RegExCaptureResult<Re extends string> =
+    Re extends ""
+	    ? {}
+	    : Re extends `(?<${infer key}>${infer rest}` // `(?<{key}>{rest}`
+	    	? rest extends `${infer _})${infer rest}` // `(?<{key}>{_}){rest'}`
+	    		? rest extends `?${infer rest}` | `*${infer rest}` // `(?<{key}>{_})?{rest''}`
+	    			? { [k in key]: string | undefined } & RegExCaptureResult<rest>
+	    		    : { [k in key]: string } & RegExCaptureResult<rest>
+                : never
+            : Re extends `${infer _}(?<${infer rest}`
+                ? RegExCaptureResult<`(?<${rest}`>
+                : {}
+
+export type RegExMatchResult<Re extends string> = {
+	matched: boolean
+	groups: RegExCaptureResult<Re> | undefined
+	raw: RegExpExecArray | undefined
+}
+
+export type RegExMatchAllResult<Re extends string> = {
+	groups: RegExCaptureResult<Re> | undefined
+	raw: RegExpMatchArray
+}[]
+
+export class TypedRegExT<Re extends string> {
+	private _regexString: Re
+	private _flags: string
+
+	private getRegex(): RegExp {
+		return new RegExp(this._regexString, this._flags)
+	}
+
+	constructor(re: Re, flags: string = "") {
+		this._regexString = re
+		this._flags = flags
+	}
+
+	isMatch = (str: string): boolean => this.getRegex().test(str)
+
+	match = (str: string): RegExMatchResult<Re> => {
+		const raw = this.getRegex().exec(str) ?? undefined
+		return {
+			matched: !!raw,
+			groups: raw?.groups as any,
+			raw,
+		}
+	}
+
+	matchAll = (str: string): RegExMatchAllResult<Re> => {
+		const re = this.getRegex()
+		return Array
+			.from(str.matchAll(re))
+			.map((raw) => ({ groups: raw.groups as any, raw }))
+	}
+
+	captures = (str: string): RegExCaptureResult<Re> | undefined => this.match(str).groups
+
+	captureAll = (str: string): (RegExCaptureResult<Re> | undefined)[] =>
+		(this.matchAll(str) as any).map((r: any) => r.groups)
+}
+
+export const TypedRegEx = <Re extends string, Fl extends string>(
+	re: Re,
+	flags?: FlagChecker<Fl> & Fl,
+) => new TypedRegExT(re, flags)
