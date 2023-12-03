807403668ccf98cf990096fcef4ecf96ab569991
scarf <greenscarf005@gmail.com>
2023-12-03 13:05:24 +0900

    docs: inline documentation

--- main.ts
+++ main.ts
@@ -53,13 +53,120 @@ export type TypedRegEx<Re extends string, Flag extends string> = {
 	regexString: Re
 	flags?: FlagChecker<Flag> & Flag | undefined
 
+	/**
+	 * Check if a string matches regex
+	 * (Equivalent to [`RegExp#test`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/RegExp/test)).
+	 *
+	 * ## Example
+	 *
+	 * ```ts
+	 * const r = typedRegEx("^(?<year>\\d{4})-(?<month>\\d{2})-(?<day>\\d{2})$")
+	 *
+	 * assertEquals(r.isMatch("2020-12-02"), true)
+	 * assertEquals(r.isMatch("2020-12"), false)
+	 * ```
+	 */
 	isMatch: (str: string) => boolean
+
+	/**
+	 * Equivalent to calling [`RegExp#exec`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/RegExp/exec) once.
+	 *
+	 * ## Example
+     *
+	 * ```ts
+	 * const dataRegex = typedRegEx("^(?<year>\\d{4})-(?<month>\\d{2})-(?<day>\\d{2})$")
+	 * const result = dataRegex.match("2020-12-02")
+	 *
+	 * assertEquals({ ...result, raw: [...result.raw!] }, {
+	 * 	matched: true,
+	 * 	raw: ["2020-12-02", "2020", "12", "02"],
+	 * 	groups: {
+	 * 		year: "2020",
+	 * 		month: "12",
+	 * 		day: "02",
+	 * 	},
+	 * })
+	 * ```
+	 *
+	 * @return the matched result with typed capture groups of type {@link RegExMatchResult}.
+	 */
 	match: (str: string) => RegExMatchResult<Re>
+
+	/**
+	 * Equivalent to [`String#matchAll`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/matchAll). Returns a list of matched results with typed capture groups.
+	 *
+	 * ## Example
+     * 
+	 * ```ts
+	 * const namesRegex = typedRegEx(
+	 * 	"((?<firstName>\\w+) (?<middleName>\\w+)? (?<lastName>\\w+))+",
+	 * 	"g",
+	 * )
+	 *
+	 * const result = namesRegex.matchAll("Joe  Mama,Ligma  Bolz,Sir Prysing Lee")
+	 *
+	 * // skips raw value testing
+	 * assertEquals(result.map(({ groups }) => groups), [
+	 * 	{ firstName: "Joe", middleName: undefined, lastName: "Mama" },
+	 * 	{ firstName: "Ligma", middleName: undefined, lastName: "Bolz" },
+	 * 	{ firstName: "Sir", middleName: "Prysing", lastName: "Lee" },
+	 * ])
+	 * ```
+	 *
+	 * @returns the an array of matched result with typed capture groups of type {@link RegExMatchAllResult}.
+	 */
 	matchAll: (str: string) => RegExMatchAllResult<Re>
+
+	/**
+	 * Extract the typed capture groups from the regular expression.
+	 *
+	 * ## Example
+	 *
+	 * ```ts
+	 * const regex = typedRegEx("^(?<year>\\d{4})-(?<month>\\d{2})-(?<day>\\d{2})$", "g")
+	 * const result = regex.captures("2020-12-02")
+	 *
+	 * result //=> { year: string, month: string, day: string } | undefined
+	 * ```
+	 *
+	 * @return capture groups of type {@link RegExCaptureResult}
+	 */
 	captures: (str: string) => RegExCaptureResult<Re> | undefined
+
+	/**
+	 * Extract all the capture groups from the regular expression as an array of typed results.
+	 *
+	 * ## Example
+	 *
+	 * ```ts
+	 * const namesRegex = typedRegEx(
+	 *   "((?<firstName>\\w+) (?<middleName>\\w+)? (?<lastName>\\w+))+",
+	 *   "g",
+	 * )
+	 * const result = namesRegex.captureAll("Joe  Mama,Ligma  Bolz,Sir Prysing Lee")
+	 * assertEquals(result, [
+	 *   { firstName: "Joe", middleName: undefined, lastName: "Mama" },
+	 *   { firstName: "Ligma", middleName: undefined, lastName: "Bolz" },
+	 *   { firstName: "Sir", middleName: "Prysing", lastName: "Lee" },
+	 * ])
+	 * ```
+	 *
+	 * @return an array of typed capture groups.
+	 */
 	captureAll: (str: string) => (RegExCaptureResult<Re> | undefined)[]
 }
 
+/**
+ * Type-safe regular expressions matcher.
+ *
+ * @param regexString Regular expression as a string literal.
+ *                    The capture groups in the regex are used to construct the type.
+ *                    (example: `(?<name>\\w+)`)
+ *
+ * @param flags [RegExp flags](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions#advanced_searching_with_flags)
+ *              as a string literal. The flags are type checked so any invalid flag characters will result in a typescript error.
+ *              (example: `gmi`)
+ */
 export const typedRegEx = <const Re extends string, const Flag extends string>(
 	regexString: Re,
 	flags?: FlagChecker<Flag> & Flag,
