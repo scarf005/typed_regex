b4edab1a82b954011deec9908e787acde91f76bb
scarf <greenscarf005@gmail.com>
2024-02-17 22:44:52 +0900

    docs: contribution & doctest (#10)

--- mod.ts
+++ mod.ts
@@ -21,12 +21,12 @@ import type { TypedRegEx } from "./api_types.ts"
  * ```ts
  * import { typedRegEx } from "https://deno.land/x/typed_regex@$MODULE_VERSION/mod.ts"
  * import { assertType, IsExact } from "https://deno.land/std@0.216.0/testing/types.ts"
- * 
+ *
  * const regex = typedRegEx("^(?<year>\\d{4})-(?<month>\\d{2})-(?<day>\\d{2})$", "g")
  * const result = regex.captures("2020-12-02")
- * 
+ *
  * assertType<IsExact<typeof result, Result>>(true)
- * 
+ *
  * type Result = { year: string; month: string; day: string } | undefined
  * ```
  */
